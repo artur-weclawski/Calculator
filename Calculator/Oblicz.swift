0c36cc1a7004f12b44f7dbcb21cc7b24230df662
Artur Węcławski <weclawski.artur@gmail.com>
2023-10-03 17:46:11 +0200

    Widok

---
+++ Calculator/Oblicz.swift
@@ -0,0 +1,18 @@
+//
+//  Oblicz.swift
+//  Calculator
+//
+//  Created by student on 03/10/2023.
+//
+
+import SwiftUI
+
+struct Oblicz: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Oblicz()
+}
